7b2d754eabe21e1ba7ab0213ba1da5541e27d0b0
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-25 18:32:30 +0100

    Registered sync packet for soup ingredients

--- common/src/main/java/com/rabbitminers/druidry/networking/DruidryNetworkHandler.java
+++ common/src/main/java/com/rabbitminers/druidry/networking/DruidryNetworkHandler.java
@@ -1,11 +1,12 @@
 package com.rabbitminers.druidry.networking;
 
 import com.rabbitminers.druidry.Druidry;
+import com.rabbitminers.druidry.content.soup.data.SoupIngredientsManager;
 
 public class DruidryNetworkHandler {
     public static final ChannelHandler CHANNEL = ChannelHandler.createChannel(Druidry.asResource("main"));
 
     public static void register() {
-        // CHANNEL.register(NetworkDir.PLAY_TO_CLIENT, SoupIngredientsManager.SyncPacket.class, SoupIngredientsManager.SyncPacket::new);
+        CHANNEL.register(NetworkDir.PLAY_TO_CLIENT, SoupIngredientsManager.SyncPacket.class, SoupIngredientsManager.SyncPacket::new);
     }
 }
