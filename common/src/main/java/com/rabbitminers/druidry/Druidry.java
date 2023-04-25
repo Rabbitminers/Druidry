0face3d2742c93157d89ce61dbe6748907936888
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-25 18:30:59 +0100

    Removed deprecated SoupIngredient registration

--- common/src/main/java/com/rabbitminers/druidry/Druidry.java
+++ common/src/main/java/com/rabbitminers/druidry/Druidry.java
@@ -1,13 +1,19 @@
 package com.rabbitminers.druidry;
 
+import com.mojang.logging.LogUtils;
 import com.rabbitminers.druidry.base.registrate.DruidicRegistrate;
 import com.rabbitminers.druidry.networking.DruidryNetworkHandler;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+
 
 public class Druidry {
     public static final String MOD_ID = "druidry";
     public static final String MOD_NAME = "Druidry";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
 
     public static void init() {
         DruidryNetworkHandler.register();
--- common/src/main/java/com/rabbitminers/druidry/base/registrate/DruidicRegistrate.java
+++ common/src/main/java/com/rabbitminers/druidry/base/registrate/DruidicRegistrate.java
@@ -1,22 +1,10 @@
 package com.rabbitminers.druidry.base.registrate;
 
-import com.rabbitminers.druidry.base.registrate.builders.SoupIngredientBuilder;
 import com.tterrag.registrate.AbstractRegistrate;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 
 public abstract class DruidicRegistrate extends AbstractRegistrate<DruidicRegistrate> {
     protected DruidicRegistrate(String modid) {
         super(modid);
     }
 
-    public SoupIngredientBuilder soupIngredient(Item item) {
-        ResourceLocation location = Registry.ITEM.getKey(item);
-        return soupIngredient(location);
-    }
-
-    public SoupIngredientBuilder soupIngredient(ResourceLocation item) {
-        return SoupIngredientBuilder.create(item);
-    }
 }
