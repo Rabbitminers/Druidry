26deed671d88f1fae33cfff53fef672cbe577dd7
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-29 21:09:39 +0100

    Implemented Fancy Texture Loaders

---
+++ common/src/main/java/com/rabbitminers/druidry/registry/DruidryTextures.java
@@ -0,0 +1,25 @@
+package com.rabbitminers.druidry.registry;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.rabbitminers.druidry.Druidry;
+import net.minecraft.resources.ResourceLocation;
+
+public enum DruidryTextures {
+    BLANK("blank.png")
+    ;
+
+    public static final String ASSET_PATH = "textures/fancy/";
+    private final ResourceLocation location;
+
+    private DruidryTextures(String filename) {
+        location = Druidry.asResource(ASSET_PATH + filename);
+    }
+
+    public void bind() {
+        RenderSystem.setShaderTexture(0, location);
+    }
+
+    public ResourceLocation getLocation() {
+        return location;
+    }
+}
