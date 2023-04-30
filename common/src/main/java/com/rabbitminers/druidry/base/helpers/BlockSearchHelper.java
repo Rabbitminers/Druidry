5303cafcf5cc8b4a230a937ac1f99421cd2acfa9
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-30 22:35:02 +0100

    Implemented condition checking for BlockSearchHelper

--- common/src/main/java/com/rabbitminers/druidry/base/helpers/BlockSearchHelper.java
+++ common/src/main/java/com/rabbitminers/druidry/base/helpers/BlockSearchHelper.java
@@ -1,18 +1,17 @@
 package com.rabbitminers.druidry.base.helpers;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BlockSearchHelper {
-    public static Set<BlockPos> getConnectedBlocks(Level world, BlockPos pos, Block targetBlock) {
+    public static Set<BlockPos> getConnectedBlocks(Level world, BlockPos pos, BlockCondition condition) {
         Set<BlockPos> connectedBlocks = new HashSet<>();
 
         Queue<BlockPos>queue = new LinkedList<>();
@@ -20,10 +19,8 @@ public class BlockSearchHelper {
 
         while (!queue.isEmpty()) {
             BlockPos currentPos = queue.poll();
-            if (!world.getBlockState(currentPos).getBlock().equals(targetBlock)) {
+            if (!(condition.validate(world, currentPos)))
                 continue;
-            }
-
             connectedBlocks.add(currentPos);
             Set<BlockPos> neighbors = getNeighborPositions(currentPos)
                     .filter(neighborPos -> !connectedBlocks.contains(neighborPos) && !queue.contains(neighborPos))
@@ -38,4 +35,30 @@ public class BlockSearchHelper {
         return Stream.of(pos.north(), pos.east(), pos.south(), pos.west(), pos.above(), pos.below());
     }
 
+    public static Set<BlockPos> getConnectedBlocks(Level world, BlockPos pos) {
+        BlockState origin = world.getBlockState(pos);
+        return BlockSearchHelper.getConnectedBlocks(world, pos, (level, location) -> {
+            BlockState block = level.getBlockState(location);
+            return block.getBlock() == origin.getBlock();
+        });
+    }
+
+    public static Set<BlockPos> getConnectedBlocks(Level world, BlockPos pos, Block block) {
+        return BlockSearchHelper.getConnectedBlocks(world, pos, (level, location) -> {
+            BlockState foundBlock = level.getBlockState(location);
+            return foundBlock.getBlock() == block;
+        });
+    }
+
+    public static Set<BlockPos> getConnectedBlocks(Level world, BlockPos pos, TagKey<Block>... tags) {
+        return BlockSearchHelper.getConnectedBlocks(world, pos, (level, location) -> {
+            BlockState foundBlock = level.getBlockState(location);
+            return Arrays.stream(tags).anyMatch(foundBlock::is);
+        });
+    }
+
+    @FunctionalInterface
+    public interface BlockCondition {
+        public boolean validate(Level world, BlockPos blockPos);
+    }
 }
