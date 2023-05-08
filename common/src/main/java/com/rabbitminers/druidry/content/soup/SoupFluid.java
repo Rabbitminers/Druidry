6983cf853f109d12c230678e4d7495f9677af64c
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-05-08 12:45:28 +0100

    Added base fluid to soup fluid

--- common/src/main/java/com/rabbitminers/druidry/content/soup/SoupFluid.java
+++ common/src/main/java/com/rabbitminers/druidry/content/soup/SoupFluid.java
@@ -5,23 +5,26 @@ import com.rabbitminers.druidry.multiloader.fluid.FluidHandler;
 import com.rabbitminers.druidry.multiloader.fluid.IFluidStack;
 import com.rabbitminers.druidry.multiloader.fluid.VirtualFluid;
 import com.rabbitminers.druidry.registry.DruidryFluids;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluid;
 
 import java.util.Arrays;
 
 public class SoupFluid extends VirtualFluid {
     public static final String INGREDIENTS_KEY = "Ingredients";
+    public static final String BASE_FLUID_KEY = "BaseFluid";
 
     public SoupFluid(Properties properties) {
         super(properties);
-        System.out.println("Created soup");
     }
 
-    public static IFluidStack create(int ammount) {
-        CompoundTag nbt = SoupFluid.createSoupData();
-        return FluidHandler.newFluidHolder(DruidryFluids.SOUP.get(), ammount, nbt);
+    public static IFluidStack create(IFluidStack base) {
+        CompoundTag nbt = SoupFluid.createSoupData(base.getFluid());
+        return FluidHandler.newFluidHolder(DruidryFluids.SOUP.get(), base.getFluidAmount(), nbt);
     }
 
     public static IFluidStack addIngredients(IFluidStack stack, SoupIngredient... ingredients) {
@@ -43,9 +46,11 @@ public class SoupFluid extends VirtualFluid {
         return nbt.getList(INGREDIENTS_KEY, ListTag.TAG_LIST);
     }
 
-    private static CompoundTag createSoupData() {
+    private static CompoundTag createSoupData(Fluid base) {
         CompoundTag nbt = new CompoundTag();
+        ResourceLocation fluidName = Registry.FLUID.getKey(base);
         nbt.put(INGREDIENTS_KEY, new ListTag());
+        nbt.putString(BASE_FLUID_KEY, fluidName.toString());
         return nbt;
     }
 }
